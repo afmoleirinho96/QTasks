66674b9173d33e6051ab97022e507ffe6c748257
André Fonseca Moleirinho <38258408+afmoleirinho96@users.noreply.github.com>
2018-08-06 21:34:41 +0100

    My First Commit of QTasks

---
+++ src/main/java/com/QTasks/QTasks/QTasksApplication.java
@@ -0,0 +1,12 @@
+package com.QTasks.QTasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QTasksApplication.class, args);
+	}
+}
