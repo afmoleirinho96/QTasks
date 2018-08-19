fc6c84bf25dc1e3d2aebcdf5b3bd9de5e0739056
André Fonseca Moleirinho <38258408+afmoleirinho96@users.noreply.github.com>
2018-08-19 18:54:54 +0100

    add sonarcloud and Travis

---
+++ bin/main/data.sql
@@ -0,0 +1,10 @@
+Describe task_item;
+
+INSERT INTO task_item (id ,description) 
+VALUES(1001, 'Work for project');
+
+
+SELECT * FROM task_item;
+
+
+show databases;
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE taskss (
+  id          BIGINT(20) PRIMARY KEY,
+  description VARCHAR(266) NOT NULL
+);
